6c5f5da6363f0caa677474a2a42f8bf45abdc92e
GDimitroulakos <dhmhgre@gmail.com>
2024-05-21 11:50:13 +0300

    Create an example with addition of two reals given from the console and print the result to the screen

--- FPUInstructions/main.asm
+++ FPUInstructions/main.asm
@@ -5,6 +5,9 @@ INCLUDE Irvine32.inc
 .data
 bigValue REAL10 1.212342342234234243E+864
 farray REAL8 1.0, 2.0, 3.0, 4.0, 5.0
+temp1 REAL4 ?
+temp2 REAL4 ?
+
 .code
 main PROC
 
@@ -31,7 +34,20 @@ fldpi
 ; reset FPU
 finit
 
+; immediate values in FPU instructions
+; is generates assembler error
+; Only way to get a float into FPU is 
+; through 
+; 1) variable declaration
+; 2) ReadFloat from console but again requires variable
+;fld 15.2
 
+; read float from console and stores it in ST(0)
+call ReadFloat
+call ReadFloat
+fadd
+; prints the contents of ST(0) to console
+call WriteFloat
 
 
 
